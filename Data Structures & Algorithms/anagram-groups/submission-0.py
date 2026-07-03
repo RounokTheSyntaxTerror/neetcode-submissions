2851ad26825901ba937634c44ed37ff46a3e98fa
RounokTheSyntaxTerror <rounok.rabbi31@gmail.com>
2026-07-03 21:17:29 +0600

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map = {}
+
+        for i in range(len(strs)):
+            key = "".join(sorted(strs[i]))
+            if key in hash_map:
+                hash_map[key].append(strs[i])
+            else:
+                hash_map[key] = [strs[i]]
+
+        return list(hash_map.values())
